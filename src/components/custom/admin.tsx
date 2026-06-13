1a7622b857e46e63006efbdb3610c9adfc6b74d1
RogerTheRabbit <34873229+RogerTheRabbit@users.noreply.github.com>
2026-06-13 12:47:00 -0400

    Make JSON easier to look at

--- src/components/custom/admin.tsx
+++ src/components/custom/admin.tsx
@@ -1,26 +1,28 @@
 import { useEffect, useState } from "react"
 import { Button } from "../ui/button"
 import { Field, FieldLabel } from "../ui/field"
-import { Textarea } from "../ui/textarea"
 import { useNavigate } from "react-router"
 import { Toaster } from "../ui/sonner"
 import { toast } from "sonner"
+import { AutosizeTextarea } from "./autosize-text-area"
 
 function Admin() {
   const [data, setData] = useState<string>("")
   const navigate = useNavigate()
 
   useEffect(() => {
-    setData(localStorage.getItem("graph") || "")
+    setData(
+      JSON.stringify(JSON.parse(localStorage.getItem("graph") || ""), null, 2)
+    )
   }, [])
 
   return (
     <div className="flex h-screen items-center justify-center">
-      <div className="grid w-full max-w-sm gap-4">
+      <div className="grid max-h-screen w-full max-w-lg gap-4">
         <Field>
           <FieldLabel>Graph</FieldLabel>
         </Field>
-        <Textarea
+        <AutosizeTextarea
           placeholder="Paste JSON here"
           value={data}
           onChange={(e) => setData(e.target.value)}
